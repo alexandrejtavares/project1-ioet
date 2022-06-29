ff7bb4f49a7fda91997d8fd9aec469820e92dd8a
Alexandre Tavares <alexandrejtavares@yahoo.com.br>
2022-06-28 23:55:41 -0300

    Update version

--- Payment.py
+++ Payment.py
@@ -1,68 +1,58 @@
-#!python3
+import logging
+from datetime import datetime, timedelta
 
-class Payment:
-
-    employee_worked_hours_dict = {}
-    employees_payments = {}
-    days_monday_friday = ["MO", "TU", "WE", "TH", "FR"]
-    days_saturday_sunday = ["SA", "SU"]
-    
-
+class Payment:   
     def __init__(self, employee_worked_hour_dict) -> dict:
-        self.employee_worked_hours_dict = employee_worked_hour_dict
+        self.employee_worked_hours_dict = employee_worked_hour_dict     
+        self.days_monday_friday = ["MO", "TU", "WE", "TH", "FR"]
+        self.days_saturday_sunday = ["SA", "SU"]
 
     def sum_payment(self, start_time, end_time, values_dict) -> float:
-        from datetime import datetime
-
-        employee_payment_total = 0
-
         for values_dict_item in values_dict:
+            sum_payment_value = 0
             start_time_values = datetime.strptime(values_dict_item["start"], '%H:%M')
             end_time_values = datetime.strptime(values_dict_item["end"], '%H:%M')
 
+            time_0000 = datetime.strptime("00:00", '%H:%M')
+            one_minute = timedelta(minutes=1)
+
+            start_time_values -= one_minute
+
+            if (end_time_values == time_0000):
+                end_time_values = datetime.strptime("23:59", '%H:%M')
+
             if start_time >= start_time_values and end_time <= end_time_values:
                 hours_worked = end_time - start_time
-                employee_payment_total += (int(hours_worked.seconds)/3600) * (int(values_dict_item["value"]))
+                payment = (int(hours_worked.seconds)/3600) * (int(values_dict_item["value"]))
+                sum_payment_value += payment
+                logging.debug(f"Start: {start_time}. End: {end_time}. Hours worked: {hours_worked}. Hour Value: {values_dict_item['value']}. Payment: {payment}. Payment Total: {sum_payment_value + self.employee_payment_total}")
+                break
+            elif start_time < end_time_values:
+                hours_worked = end_time_values - start_time
+                payment = (int(hours_worked.seconds)/3600) * (int(values_dict_item["value"]))
+                sum_payment_value += payment
+                logging.debug(f"Start: {start_time}. End: {end_time}. Hours worked: {hours_worked}. Hour Value: {values_dict_item['value']}. Payment: {payment}. Payment Total: {sum_payment_value + self.employee_payment_total}")
+                start_time = end_time_values
 
-        return employee_payment_total
+        return sum_payment_value
 
     def calculate_payment(self, working_hours_values_monday_friday_dict, working_hours_values_saturday_sunday_dict):
-        from datetime import datetime
-
-        employee_payment_dict = {}
         for employee in self.employee_worked_hours_dict:
-            print(employee)
             employee_days_dict = self.employee_worked_hours_dict[employee]
-            print(employee_days_dict)
-            employee_payment_total = 0
+            self.employee_payment_total = 0
+        
             for day in employee_days_dict:
-                print(day)
+                logging.debug(f"Calculating values from employee {employee} from day {day}.")
                 day_dict = employee_days_dict[day]
                 
                 start_time = datetime.strptime(day_dict["start"], '%H:%M')
                 end_time = datetime.strptime(day_dict["end"], '%H:%M')
 
                 if day in self.days_monday_friday:
-                    employee_payment_total += self.sum_payment(start_time, end_time, working_hours_values_monday_friday_dict)
-                    # for values_monday_friday in working_hours_values_monday_friday_dict:
-                    #     start_time_values = datetime.strptime(values_monday_friday["start"], '%H:%M')
-                    #     end_time_values = datetime.strptime(values_monday_friday["end"], '%H:%M')
-
-                    #     if start_time >= start_time_values and end_time <= end_time_values:
-                    #         hours_worked = end_time - start_time
-                    #         employee_payment_total += (int(hours_worked.seconds)/3600) * (int(values_monday_friday["value"]))
+                    self.employee_payment_total += self.sum_payment(start_time, end_time, working_hours_values_monday_friday_dict)
                 else:
-                    employee_payment_total += self.sum_payment(start_time, end_time, working_hours_values_saturday_sunday_dict)
-                    # for values_saturday_sunday in working_hours_values_saturday_sunday_dict:
-                    #     start_time_values = datetime.strptime(values_saturday_sunday["start"], '%H:%M')
-                    #     end_time_values = datetime.strptime(values_monday_friday["end"], '%H:%M')
+                    self.employee_payment_total += self.sum_payment(start_time, end_time, working_hours_values_saturday_sunday_dict)
 
-                    #     if start_time >= start_time_values and end_time <= end_time_values:
-                    #         hours_worked = end_time - start_time
-                    #         employee_payment_total += (int(hours_worked.seconds)/3600) * (int(values_monday_friday["value"]))
-
-                        #start_time_values = datetime.strptime(working_hours_values_monday_friday_dict[values_monday_friday], '%H:%M')
-                        #end_time_values = datetime.strptime(working_hours_values_monday_friday_dict[values_monday_friday], '%H:%M')
-            print(f"{employee}: USD {employee_payment_total}")
+            print(f"The amount to pay {str.upper(employee)} is: {self.employee_payment_total} USD")
                     
         
--- main.py
+++ main.py
@@ -1,29 +1,27 @@
 #!python3
 
 import json
-from signal import raise_signal
-import string
-import re
+import logging
 from Payment import Payment
+import re
 
-def read_json_file(json_file_name: string):
+def read_json_file(json_file_name: str):
     try:
         json_file = open(json_file_name, "r")    
         return json.load(json_file)
     except:
-        print (f"Error reading json file {json_file_name}")
+        logging.critical("Error reading json file {json_file_name}")
 
-def read_txt_file(txt_file_name: string):
+def read_txt_file(txt_file_name: str):
     try:
         txt_file_content = open(txt_file_name, "r").read().splitlines() 
-
         return txt_file_content
     except:
-        print (f"Error reading txt file {txt_file_name}")
+        logging.critical(f"Error reading txt file {txt_file_name}")
 
 def get_values_from_working_hours_file(file_name):
     working_hour_values = read_json_file(file_name)
-
+    logging.info(f"Reading values from working hours file: {file_name}")
     return working_hour_values
 
 def get_values_from_employees_worked_hours_file(file_name):
@@ -31,41 +29,32 @@ def get_values_from_employees_worked_hours_file(file_name):
     
     return employees_hours_worked
 
-def get_working_hour_values_from_period(period, working_hour_values_file_name):
-
-    working_hour_values_list = get_values_from_working_hours_file(working_hour_values_file_name)
+def get_working_hour_values_from_period(period, working_hour_values_list):   
     try:
         working_hour_values_from_period = working_hour_values_list[period]
         return working_hour_values_from_period
     except:
-        print (f"Error getting working hour values from period: {period}")
+        logging.critical(f"Error getting working hour values from period: {period}.")
 
-def get_employees_worked_hours_dict(employees_hours_worked_file_name):
-    
+def get_employees_worked_hours_dict(employees_worked_hours_list):    
     employee_worked_hours_dict = {}
     worked_hours_regex = r"([0-9]{2}:[0-9]{2}-[0-9]{2}:[0-9]{2})"
     worked_days_regex = r"[a-zA-Z]{2}"
 
-    employees_worked_hours_list = get_values_from_employees_worked_hours_file(employees_hours_worked_file_name)
-
     for employees_worked_hours_item in employees_worked_hours_list:
-        
         worked_hours_dict = {}
 
         # Employee name
-        employee_name = employees_worked_hours_item.partition("=")[0]
-        print(employee_name)
-
+        employee_name = employees_worked_hours_item.partition("=")[0]     
         worked_info = employees_worked_hours_item.partition("=")[2]
-        print(worked_info)
 
         # Worked days
         worked_days = re.findall(worked_days_regex, worked_info)
 
         # Worked hours
         worked_hours = re.findall(worked_hours_regex, worked_info)
-
         index = 0       
+
         while index < len(worked_days):
             worked_hours_dict[worked_days[index]] = json.loads(f"{{\"start\": \"{worked_hours[index][:5]}\", \"end\": \"{worked_hours[index][6:]}\"}}")
             index += 1
@@ -74,14 +63,25 @@ def get_employees_worked_hours_dict(employees_hours_worked_file_name):
 
     return employee_worked_hours_dict
 
+log_level = getattr(logging, "INFO", None) # CRITICAL, ERROR, WARNING, INFO, DEBUG, NOTSET
+
+logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=log_level)
+
 working_hour_values_file_name = "working_hour_values.json" 
 employees_hours_worked_file_name = "input_employees_worked_hours.txt"
 
-working_hours_values_monday_friday_dict = get_working_hour_values_from_period("monday-friday", working_hour_values_file_name)
-working_hours_values_saturday_sunday_dict = get_working_hour_values_from_period("saturday-sunday", working_hour_values_file_name)
+# Read JSON file 
+working_hour_values_list = get_values_from_working_hours_file(working_hour_values_file_name)
 
-employee_worked_hours_dict = get_employees_worked_hours_dict(employees_hours_worked_file_name)
+# Get the values of worked hours of week and weekend as dicts
+working_hours_values_monday_friday_dict = get_working_hour_values_from_period("monday-friday", working_hour_values_list)
+working_hours_values_saturday_sunday_dict = get_working_hour_values_from_period("saturday-sunday", working_hour_values_list)
+
+# Read employees worked hours input file
+employees_worked_hours_list = get_values_from_employees_worked_hours_file(employees_hours_worked_file_name)
+
+# Get the total hours worked by an employee as dict
+employee_worked_hours_dict = get_employees_worked_hours_dict(employees_worked_hours_list)
 
 payment_obj = Payment(employee_worked_hours_dict)
 payment_obj.calculate_payment(working_hours_values_monday_friday_dict, working_hours_values_saturday_sunday_dict)
-
